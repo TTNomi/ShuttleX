35bf06f34a5c21a301d2f93ab97f8e2229e29ffc
sipt <1182102908@qq.com>
2019-09-18 00:50:40 +0800

    Initial Commit

---
+++ shuttleX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  shuttleX
+//
+//  Created by sipt on 2019/9/18.
+//  Copyright © 2019 sipt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
