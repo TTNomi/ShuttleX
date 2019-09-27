64ae8cc5561afd145afa37e5d641d8243da0fdde
sipt <1182102908@qq.com>
2019-09-28 03:17:37 +0800

    Initial Commit

---
+++ ShuttleX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ShuttleX
+//
+//  Created by sipt on 2019/9/28.
+//  Copyright © 2019 sipt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
